11779733499aa96f77d7c730ddbcb49f8afc4fd2
saurabh <saurabhpathak184@gmail.com>
2024-03-06 22:59:27 +0530

    connected to mongoDB

--- Readme.md
+++ Readme.md
@@ -1,3 +1,17 @@
 # SETUP PROFESSIONAL BACKEND PROJECT
 
+- npm init
+- create folder 'public' => 'temp' => .gitkeep , 
+beacuse git don't keep record of empty folders so by including 'public' it will track it.
+
+- npm i dotenv => create file  '.env'
+- create file '.gitignore'  , use git ignore generator (use Node)
+- to push .env on git, it is not recommended => so use .env.sample to push it on git.
+
+- in root => create folder 'src'
+- shortcut => mkdir folderName 
+
+- in src => mkdir controllers db middlewares models routes utils
+- npm i -D prettier => create file '.prettierrc' => create file '.prettierignore'
+
 
---
+++ src/db/database.js
@@ -0,0 +1,52 @@
+// 1ST APPROACH - CONNECT DB IN index.js , but it pollutes the index.js file
+
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import { DB_NAME } from './constants';
+
+// const app = express();
+
+// ;(async ()=>{
+//     try{   
+//         await mongoose.connect(`${process.env.MONGODB_URI}`,{
+//             dbName: DB_NAME
+//         })
+
+//         app.on('error',(error)=>{
+//             console.log("ERROR: ",error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT || 3000,()=>{
+//             console.log("Server is running on port: " + process.env.PORT);
+//         })
+//     }
+//     catch(err){
+//         console.log("ERROR: ",err);
+//         throw err;
+//     }
+
+// }
+// )()
+
+// ===============================================================================================================
+
+
+// 2ND APPROACH - CONNECT DB IN db=> database.js and connect it in index.js
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB;
